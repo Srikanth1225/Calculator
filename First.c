9875185f1c6da403343b42e9831a9c5931592535
Kishore19092003 <kishoreshekar1909@gmail.com>
2025-03-26 11:26:24 +0530

    Update First.c
    multiplication and division functions are created for two numbers

--- First.c
+++ First.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
+void mul(int n1, int n2)
+{
+return n1*n2;
+}
+void  division(int n1, int n2)
+{
+return n1/n2;
+}
 int main()
 {
-  printf("Hello world");
+  int num1, num2, res;
+  printf("Enter first number: );
+  scanf("%d",&num1);
+  printf("Enter second number: );
+  scanf("%d",&num2);
+  res=mul(num1,num2);
+  printf("The multiplication of %d and %d is %d",num1,num2,res);
+  res=division(num1,num2);
+  printf("The division of %d and %d is %d",num1,num2,res);
+ 
+  return 0;
 }
